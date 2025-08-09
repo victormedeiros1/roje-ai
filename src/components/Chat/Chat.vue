5236dfb6bd33e9e971a0bff9b365d422025a1578
José Victor <josevictordev@gmail.com>
2025-08-08 22:48:34 -0300

    feat: botoes de acoes prontas

--- src/components/Chat/Chat.vue
+++ src/components/Chat/Chat.vue
@@ -1,8 +1,16 @@
 <template>
 	<div class="chat-container" @click="fecharChat">
 		<div class="chat" @click.stop>
-			<div class="ghost"></div>
-			<h1 class="chat__title" ref="tituloDoChat">Olá</h1>
+			<div class="chat__acoes-prontas" ref="acoesProntas">
+				<Button
+					class="chat__acao-pronta"
+					text
+					v-for="acao in acoesProntasTexto"
+					:key="acao"
+					:label="acao"
+				/>
+			</div>
+			<h1 class="chat__title" ref="tituloDoChat">Olá, eu sou o Roje!</h1>
 			<textarea class="chat__campo" ref="campoDeTexto" />
 		</div>
 	</div>
@@ -10,6 +18,7 @@
 
 <script setup lang="ts">
 import Container from '@/components/Container/Container.vue'
+import Button from 'primevue/button'
 import { useAnimations } from '@/animations/animations'
 import { onMounted, ref, defineEmits } from 'vue'
 
@@ -19,14 +28,22 @@ const emit = defineEmits<{ fechar: [] }>()
 
 const tituloDoChat = ref<HTMLElement | null>(null)
 const campoDeTexto = ref<HTMLElement | null>(null)
+const acoesProntas = ref<HTMLElement | null>(null)
 
 const fecharChat = () => {
 	emit('fechar')
 }
 
+const acoesProntasTexto = [
+	'Me dê dicas do que fazer com meu banco de horas',
+	'Quero tirar um day off esse mês',
+	'Posso sair mais cedo hoje?'
+]
+
 onMounted(() => {
-	fadeIn(tituloDoChat)
-	fadeIn(campoDeTexto)
+	fadeIn(acoesProntas, { delay: 0.2 })
+	fadeIn(tituloDoChat, { delay: 0.4 })
+	fadeIn(campoDeTexto, { delay: 0.6 })
 })
 </script>
 
@@ -55,6 +72,19 @@ onMounted(() => {
 	border: 4px solid var(--gray-200);
 	backdrop-filter: blur(12px);
 	padding: var(--p-16);
+
+	&__acoes-prontas {
+		display: flex;
+		flex-wrap: wrap;
+		gap: var(--p-8);
+	}
+
+	&__acao-pronta {
+		font-size: var(--fs-14);
+		border-radius: 6px;
+		border: 1px solid var(--gray-300);
+		padding: var(--p-8);
+	}
 
 	&__campo {
 		width: 100%;
